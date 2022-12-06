129276ac7ccfce834d8ef312faddccddbeff7f38
drs317lakshami <devrajkumar317@gmail.com>
2022-12-06 21:52:13 +0530

    Welcome to address book

---
+++ src/com/bridgelab/AddressBookUsingCollection.java
@@ -0,0 +1,16 @@
+package com.bridgelab;
+
+/**
+ * 
+ * @author dev
+ *
+ */
+
+public class AddressBookUsingCollection {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to address book using collection");
+	}
+
+}
